793c896369ee3cbe5068c0d55bf2fcb0299fb155
IsanFaizun <113765539+IsanFaizun@users.noreply.github.com>
2024-07-01 00:08:27 +0700

    fix: add contact info

--- script.js
+++ script.js
@@ -14,17 +14,14 @@ window.addEventListener('scroll', function() {
     }
 });
 
-const list = document.querySelector(".slider");
-
-  // We want to know the width of one of the items. We'll use this to decide how many pixels we want our carousel to scroll.
+function handleClick(direction) {
+  const list = document.querySelector(".slider");
   const item = document.querySelector(".slider-item");
   const itemWidth = item.offsetWidth;
-
-  function handleClick(direction) {
-    // Based on the direction we call `scrollBy` with the item width we got earlier
-    if(direction === "previous") {
-      list.scrollBy({ left: -itemWidth, behavior: "smooth" });
-    } else {
-      list.scrollBy({ left: itemWidth, behavior: "smooth" });
-    }
+  
+  if(direction === "previous") {
+    list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+  } else {
+    list.scrollBy({ left: itemWidth, behavior: "smooth" });
   }
+}
