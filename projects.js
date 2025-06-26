5f989316f347c97c17bf3aba77d8a90da419656e
ee-son <isan.faizun.hidar@mail.ugm.ac.id>
2025-06-26 08:19:29 +0700

    fix: navbar color adjustment and add footer

--- projects.js
+++ projects.js
@@ -1,3 +1,21 @@
+// Navbar adjustment
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const toggler = document.getElementById('navbar-toggler');
+    if (window.scrollY > 50) {
+        navbar.classList.add('bg-inverse');
+        navbar.classList.remove('bg-white');
+        toggler.classList.remove('navbar-light');
+        toggler.classList.add('navbar-dark');
+    } else {
+        navbar.classList.add('bg-white');
+        navbar.classList.remove('bg-inverse');
+        toggler.classList.remove('navbar-dark');
+        toggler.classList.add('navbar-light');
+    }
+});
+
+// Project Pagination
 const itemsPerPage = 8;
 let currentPage = 1;
 
